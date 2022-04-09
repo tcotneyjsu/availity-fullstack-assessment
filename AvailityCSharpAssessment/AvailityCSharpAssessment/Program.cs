edd9cec5d1576bd893efa9278c5f43a6451f6260
Travis <traviscotney@gmail.com>
2022-04-09 16:32:31 -0500

    Created the initial Visual Studio projects for the CSharp portions of the Assessment. Finished creating the LISP Code Validator that contains a functions for checking that all parentheses are closed and properly nested.

---
+++ AvailityCSharpAssessment/AvailityCSharpAssessment/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AvailityCSharpAssessment
+{
+    class Program
+    {
+        //Adding a temp start point for testing
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program Start!");
+            // String pulled from an example on the Lisp wikipedia page
+            string lispCode = "((lambda (arg) (+ arg 1)) 5)";
+            LISPCodeValidator.LISPParenthesesChecker(lispCode);
+        }
+    }
+}
